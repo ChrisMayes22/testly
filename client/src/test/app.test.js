ec1cf50c1041c375c9e6e0425246fb3cb92d8695
Chris Mayes <coachchristac@gmail.com>
2019-03-29 18:25:34 -0400

    bugfix

--- client/src/test/app.test.js
+++ client/src/test/app.test.js
@@ -1,5 +1,12 @@
-describe('random test', () => {
-    test('random test', () => {
-        expect('Hi').toBe('Hi');
-    })
-})
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+
+it('renders without crashing', () => {
+  console.log( shallow ); 
+});
