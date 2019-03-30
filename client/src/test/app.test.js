81f55b663fa4f1d27198f8123c892beae00b7f6b
Chris Mayes <coachchristac@gmail.com>
2019-03-30 12:57:16 -0400

    Fixed bug that caused client-side testing to fail

--- client/src/test/app.test.js
+++ client/src/test/app.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../stateful/App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 
 it('renders without crashing', () => {
-  console.log( shallow ); 
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
 });
